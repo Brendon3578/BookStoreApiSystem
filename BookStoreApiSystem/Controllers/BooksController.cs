2c1c9e96eb1ce96a23e44e74b52e962ee8abcd2d
Brendon Gomes <brendongomes40@gmail.com>
2024-11-03 21:16:37 -0300

    Adicionar arquivos de projeto.

---
+++ BookStoreApiSystem/Controllers/BooksController.cs
@@ -0,0 +1,71 @@
+﻿using BookStoreApiSystem.Models;
+using BookStoreApiSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreApiSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksService _booksService;
+        public BooksController(BooksService booksService) =>
+        _booksService = booksService;
+
+        [HttpGet]
+        public async Task<List<Book>> Get() =>
+            await _booksService.GetAsync();
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null) return NotFound();
+
+            return book;
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            await _booksService.CreateAsync(newBook);
+
+            return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Book updatedBook)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+                return NotFound();
+
+            updatedBook.Id = book.Id;
+
+            await _booksService.UpdateAsync(id, updatedBook);
+
+            return NoContent();
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+                return NotFound();
+
+            await _booksService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
